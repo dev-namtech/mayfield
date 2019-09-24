4888f41680d12335a6f9731be690da7a3fe85108
Ten <ten@namtech.com.au>
2019-09-24 10:53:48 +0700

    Edit wholesale signup.

--- src/js/main.js
+++ src/js/main.js
@@ -329,7 +329,6 @@ jQuery(document).ready(function() {
     $(window).resize(function() {
         changePositionElement();
     });
-        
     
     function changePositionElement() {
         let productRange = $('.lampshades-main-content .product-range')
@@ -415,6 +414,16 @@ jQuery(document).ready(function() {
                 } else {
                     $('.p-status').html('<p>'+response.Messages.Error.Message+'</p>');
                 }
+            }
+        });
+    });
+
+    $('#wholesaleSignUpMail').find('input[type="checkbox"]').each(function(index){
+        $(this).on('click',function () {
+            if ($(this).is(':checked')) {
+                $('input[name="inp-business'+index+'"]').val($(this).val());
+            } else {
+                $('input[name="inp-business'+index+'"]').val('');
             }
         });
     });
